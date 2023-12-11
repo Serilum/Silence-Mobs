f8cd38ea1df7df840caee643d2917faf7c0e2e14
Rick South <rick@natamus.com>
2023-12-12 00:08:58 +0100

    Silence Mobs source code for 1.18.2.

---
+++ Fabric/src/main/java/com/natamus/silencemobs/ModFabric.java
@@ -0,0 +1,39 @@
+package com.natamus.silencemobs;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.fabric.callbacks.CollectiveEntityEvents;
+import com.natamus.silencemobs.cmds.CommandSt;
+import com.natamus.silencemobs.events.SilenceEvent;
+import com.natamus.silencemobs.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+			CommandSt.register(dispatcher);
+		});
+
+		CollectiveEntityEvents.ON_LIVING_ATTACK.register((Level world, Entity entity, DamageSource damageSource, float damageAmount) -> {
+			return SilenceEvent.onEntityDamage(world, entity, damageSource, damageAmount);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
