9a2edc6bf8a6358360f16b15e461c7dd714923d8
Rick South <rick@natamus.com>
2023-12-12 00:09:38 +0100

    Silence Mobs source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/silencemobs/neoforge/events/NeoForgeEntityEvent.java
@@ -0,0 +1,25 @@
+package com.natamus.silencemobs.neoforge.events;
+
+import com.natamus.silencemobs.cmds.CommandSt;
+import com.natamus.silencemobs.events.SilenceEvent;
+import net.minecraft.world.entity.Entity;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.neoforge.event.entity.living.LivingAttackEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeEntityEvent {
+	@SubscribeEvent
+	public static void registerCommands(RegisterCommandsEvent e) {
+		CommandSt.register(e.getDispatcher());
+	}
+
+	@SubscribeEvent
+	public static void onEntityDamage(LivingAttackEvent e) {
+		Entity entity = e.getEntity();
+		if (!SilenceEvent.onEntityDamage(entity.level(), entity, e.getSource(), e.getAmount())) {
+			e.setCanceled(true);
+		}
+	}
+}
