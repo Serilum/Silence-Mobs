cca2d7312c9ad539be7080960ffc7920aa8dd44d
Rick South <rick@natamus.com>
2024-06-26 00:01:22 +0200

    (v3.4) (1.21.0 Neo) Updated mod to work with NeoForge version 21.0.31+.

--- NeoForge/src/main/java/com/natamus/silencemobs/neoforge/events/NeoForgeEntityEvent.java
+++ NeoForge/src/main/java/com/natamus/silencemobs/neoforge/events/NeoForgeEntityEvent.java
@@ -3,10 +3,10 @@ package com.natamus.silencemobs.neoforge.events;
 import com.natamus.silencemobs.cmds.CommandSt;
 import com.natamus.silencemobs.events.SilenceEvent;
 import net.minecraft.world.entity.Entity;
-import net.neoforged.neoforge.event.RegisterCommandsEvent;
-import net.neoforged.neoforge.event.entity.living.LivingAttackEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
 @EventBusSubscriber
 public class NeoForgeEntityEvent {
@@ -16,7 +16,7 @@ public class NeoForgeEntityEvent {
 	}
 
 	@SubscribeEvent
-	public static void onEntityDamage(LivingAttackEvent e) {
+	public static void onEntityDamage(LivingIncomingDamageEvent e) {
 		Entity entity = e.getEntity();
 		if (!SilenceEvent.onEntityDamage(entity.level(), entity, e.getSource(), e.getAmount())) {
 			e.setCanceled(true);
