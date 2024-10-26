21134fff2eb4a127afaa8103447d26494a8f174d
Rick South <rick@natamus.com>
2024-10-26 15:00:45 +0200

    Silence Mobs source code for 1.21.3.

---
+++ Fabric/src/main/java/com/natamus/silencemobs/fabric/config/IntegrateModMenu.java
@@ -0,0 +1,13 @@
+package com.natamus.silencemobs.fabric.config;
+
+import com.natamus.collective.config.DuskConfig;
+import com.natamus.silencemobs.util.Reference;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class IntegrateModMenu implements ModMenuApi {
+	@Override
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+		return parent -> DuskConfig.DuskConfigScreen.getScreen(parent, Reference.MOD_ID);
+	}
+}
