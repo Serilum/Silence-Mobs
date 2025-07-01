13f75c1e8004d859c35677eae91774ab03032ad3
Rick South <rick@natamus.com>
2025-07-01 09:56:17 +0200

    Silence Mobs source code for 1.21.7.

---
+++ Common/src/main/java/com/natamus/silencemobs/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.silencemobs;
+
+import com.natamus.silencemobs.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
