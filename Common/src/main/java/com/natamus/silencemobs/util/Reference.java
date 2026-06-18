1fbfd2d05d099a792a084e107c11a893dfbcc334
Rick South <rick@natamus.com>
2026-06-18 22:26:29 +0200

    Updated Silence Mobs to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/silencemobs/util/Reference.java
+++ Common/src/main/java/com/natamus/silencemobs/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "silencemobs";
 	public static final String NAME = "Silence Mobs";
 	public static final String VERSION = "4.0";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
